7fa905d1907cf8b2844b471d9858cb7afb39c3ad
Mark <mark.j.kubik@gmail.com>
2017-10-11 22:27:47 +0100

    enabled parsing of JSON data from API

--- public/App.js
+++ public/App.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
 	//Get Geo location//
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-		$("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+	 if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(function(position) {
+	
+	//parsing the JSON API//
+	var lon = position.coords.longitude;
+	var lat = position.coords.latitude;
+	var weatherAPI = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon;
+	
+	$.getJSON(weatherAPI, function(data){
+		alert(JSON.stringify(data));
+			});
 		});
-		}
-		
-		
-		
+	}
 });
