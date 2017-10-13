6be0bb493955d4356e23bfda9095cf91f1ec0317
Mark <mark.j.kubik@gmail.com>
2017-10-13 21:04:21 +0100

    weather container layout completed

--- public/App.js
+++ public/App.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
-	//Get Geo location//
-	 if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(function(position) {
-	
-	//parsing the JSON API//
-	var lon = position.coords.longitude;
-	var lat = position.coords.latitude;
-	var weatherAPI = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon;
-	
-	$.getJSON(weatherAPI, function(data){
-		alert(JSON.stringify(data));
+		//Get Geo location//
+	if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(function(position){
+		//parsing the JSON API//
+		var lon = position.coords.longitude;
+		var lat = position.coords.latitude;
+		var weatherAPI = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon;
+		
+		$.getJSON(weatherAPI, function(data){
+			
+			//local weather information
+			$("#weather-icon").html(data.weather.icon);
+			$("#weather-degrees").html(Math.round(data.main.temp) + "&deg;c");
+			
+			$("#weather-humidity").html(Math.round(data.main.humidity) + "%");
+			$("#weather-wind").html(Math.round(data.wind.speed) + "mph");
+			$("#weather-location").html(data.name);
 			});
 		});
-	}
+	};
 });
+
