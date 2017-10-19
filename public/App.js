80a18d539812e2596a0ce2eb448582abe9b03df2
KUBIX90 <mark.j.kubik@gmail.com>
2017-10-19 19:27:54 +0100

    local weather from API now displaying

--- public/App.js
+++ public/App.js
@@ -8,11 +8,13 @@ $(document).ready(function(){
 		var weatherAPI = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon;
 		
 		$.getJSON(weatherAPI, function(data){
+		
+			var icon = data.weather[0].icon;
 			
 			//local weather information
-			$("#weather-icon").html(data.weather.icon);
+			$("#weather-icon").attr("src", icon);
 			$("#weather-degrees").html(Math.round(data.main.temp) + "&deg;c");
-			
+			$("#weather-description").html(data.weather[0].description);
 			$("#weather-humidity").html(Math.round(data.main.humidity) + "%");
 			$("#weather-wind").html(Math.round(data.wind.speed) + "mph");
 			$("#weather-location").html(data.name);
