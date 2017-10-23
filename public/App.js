7f487f70a256dc339719cca1dfcc701dbd1b2a15
KUBIX90 <mark.j.kubik@gmail.com>
2017-10-23 22:57:56 +0100

    click functionality added to Tokyo button, showing Tokyo weather

--- public/App.js
+++ public/App.js
@@ -9,30 +9,41 @@ $(document).ready(function(){
 		var weatherAPI = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon;
 		
 		$.getJSON(weatherAPI, function(data){
-		
-			var icon = data.weather[0].icon;
-			var temp = Math.round(data.main.temp);
 			
 			//local weather information
-			$("#weather-icon").attr("src", icon);
+			$("#weather-icon").attr("src", data.weather[0].icon);
 			$("#weather-degrees").html(Math.round(data.main.temp) + "&deg;c");
 			$("#weather-description").html(data.weather[0].description);
 			$("#weather-humidity").html(Math.round(data.main.humidity) + "%");
 			$("#weather-wind").html(Math.round(data.wind.speed) + "mph");
 			$("#weather-location").html(data.name);
-			
-				//toggle Celsuis & Fahrenheit
-				$(".slider").on("click", function(){
-		
-					$("#weather-degrees").html(Math.round(temp*9/5 + 32) + "&deg;f");
-				});
-			
-			
 			});
 		});
-		
 	};
 	
-
+	
+				//Tokyo weather on click
+			
+			$("#weather-tokyo").on("click", function(){
+				
+					var tokyoLon = 390;
+					var tokyoLat = 11;
+					var tokyoWeather = "https://fcc-weather-api.glitch.me/api/current?lat=" + tokyoLat + "&lon=" + tokyoLon;
+					
+					$(".view").css({"background": "url(public/images/andre-benz-256762.jpg)", "background-size": "cover"});
+					
+				
+				$.getJSON(tokyoWeather, function(data){
+					
+						$("#weather-icon").attr("src", data.weather[0].icon);
+						$("#weather-degrees").html(Math.round(data.main.temp) + "&deg;c");
+						$("#weather-description").html(data.weather[0].description);
+						$("#weather-humidity").html(Math.round(data.main.humidity) + "%");
+						$("#weather-wind").html(Math.round(data.wind.speed) + "mph");
+						$("#weather-location").html("Tokyo, Japan");
+					
+				});
+				
+			});	
 });
 
