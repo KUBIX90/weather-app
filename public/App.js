732e4efc77e4f6827bd6c13797de5642938be543
KUBIX90 <mark.j.kubik@gmail.com>
2018-02-18 22:27:22 +0000

    City data and background changes implemented with vanilla JS

--- public/App.js
+++ public/App.js
@@ -15,10 +15,11 @@ function getWeather(lon, lat){
 	apiRequest.send();
 }
 
-
-
-
-
+function changeBackground(city, name, link){
+	document.querySelector("html body").style.backgroundImage = "url(public/images/" + city + ".jpg)";
+	document.getElementById("image-credit").innerHTML = "Background by " + name;
+	document.getElementById("image-credit").setAttribute("href", link);
+}
 
 
 
@@ -26,18 +27,45 @@ function getWeather(lon, lat){
 //////////////////////////////////////////////////////////////////////
 
 
-//Get Geo location//
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      //Show my local weather
-      var lon = position.coords.longitude;
-      var lat = position.coords.latitude;
+//Show my local weather
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(function(position) {
+  var lon = position.coords.longitude;
+  var lat = position.coords.latitude;
 
-      getWeather(lon, lat);
-    });
-  }
+  getWeather(lon, lat);
+});
+}
 
+document.getElementById("weather-paris").addEventListener("click", function(){
+	getWeather(2.3, 48.85);
+	changeBackground("paris", "Chris Karidis", "https://unsplash.com/photos/nnzkZNYWHaU");
+});
 
+document.getElementById("weather-tokyo").addEventListener("click", function(){
+	getWeather(139.8, 35.65);
+	changeBackground("tokyo", "Andre Benz", "https://unsplash.com/photos/Mn9Fa_wQH-M");
+});
+
+document.getElementById("weather-LA").addEventListener("click", function(){
+	getWeather(-118.25, 34.05);
+	changeBackground("los_angeles", "Sasha", "https://unsplash.com/photos/WggyAvQTk6I");
+});
+
+document.getElementById("weather-dubai").addEventListener("click", function(){
+	getWeather(55.27, 25.2);
+	changeBackground("dubai", "Roman Logov", "https://unsplash.com/photos/jomuw0-3F8A");
+});
+
+document.getElementById("weather-new-york").addEventListener("click", function(){
+	getWeather(-73.94, 40.73);
+	changeBackground("new_york", "Frank Kohntopp", "https://unsplash.com/photos/yUJfcGrJDbc");
+});
+
+document.getElementById("weather-london").addEventListener("click", function(){
+	getWeather(-0.11, 51.5);
+	changeBackground("london", "Rob Bye", "https://unsplash.com/photos/7baQ2b019NY");
+}); 
 
 
 /*$(document).ready(function() {
